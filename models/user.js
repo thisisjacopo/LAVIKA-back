b3658b9b750eb8b859408cb78a2ec1170ef22611
Jacopo <jacopo.rodighiero@icloud.com>
2020-05-22 18:09:59 +0200

    FRIDAY 6PM

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {type: String, required: true, unique: true},
-  email: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
+  username: {type: String },
+  email: {type: String },
+  password: {type: String},
   imgPath: {type: String},
   songs: [{type: Schema.Types.ObjectId, ref:'Song'}]
 }, {
