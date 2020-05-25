d1e75876abd975e5921e875ed94593ada7d8c076
Sofia Santa Maria <yolaresofia@gmail.com>
2020-05-25 11:37:37 +0200

    deploy version 1

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN],
+    origin: [process.env.PUBLIC_DOMAIN, 'https://lavika-app.herokuapp.com']
   }),
 );
 // app.use((req, res, next) => {
@@ -73,6 +73,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/scenes', scenesRouter)
+
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // ERROR HANDLING
 // catch 404 and forward to error handler
---
+++ public/precache-manifest.5e625a3ca34e6a79c6f6103a631ecec4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa6d4d114e52300d5e4314cef157098",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ed7c42c7e1c7bd4b54e",
+    "url": "/static/css/main.f9fe6372.chunk.css"
+  },
+  {
+    "revision": "58d1e83180ee8eef6975",
+    "url": "/static/js/2.92420e2f.chunk.js"
+  },
+  {
+    "revision": "a07382cb04fac37177dc49920436de9c",
+    "url": "/static/js/2.92420e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed7c42c7e1c7bd4b54e",
+    "url": "/static/js/main.1590a69c.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "3fd0983585b75b63af8bcc14b678db75",
+    "url": "/static/media/Comfortaa-Medium.3fd09835.ttf"
+  },
+  {
+    "revision": "8233c19aac7baddce4714e665af60828",
+    "url": "/static/media/Mantra-Regular.8233c19a.ttf"
+  },
+  {
+    "revision": "d70b25a22acc3aa29df0d0c62b0d8af1",
+    "url": "/static/media/hh.d70b25a2.wav"
+  },
+  {
+    "revision": "95352fd45ec2f250d68390daf3317db6",
+    "url": "/static/media/k.95352fd4.wav"
+  },
+  {
+    "revision": "22cd8a7b984e17a23407097078f4eff3",
+    "url": "/static/media/o1.22cd8a7b.wav"
+  },
+  {
+    "revision": "3e96ee9f0253098a0db40460f33ea899",
+    "url": "/static/media/o2.3e96ee9f.wav"
+  },
+  {
+    "revision": "1ed731560f5588523e0d55a8582e6c18",
+    "url": "/static/media/o3.1ed73156.wav"
+  },
+  {
+    "revision": "d14a03d18340012419227102a8c55a23",
+    "url": "/static/media/o4.d14a03d1.wav"
+  },
+  {
+    "revision": "0bc6096a9f6dbd74293ad435e6c4a5d7",
+    "url": "/static/media/o5.0bc6096a.wav"
+  },
+  {
+    "revision": "62f6aa57fbec6366a12409f7d3c91f55",
+    "url": "/static/media/o6.62f6aa57.wav"
+  },
+  {
+    "revision": "aa8a91693364fa1ce4ab1e8f4e4000a1",
+    "url": "/static/media/s.aa8a9169.wav"
+  }
+]);
