5d368187cd9604c6a549cb71d00de101118430fb
Sofia Santa Maria <yolaresofia@gmail.com>
2020-05-27 16:08:44 +0200

    trying deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd9f4a1806c1abf23384b6af3fb74277.js"
+  "/precache-manifest.e8af857ecc63051904f027cc32637fed.js"
 );
 
 self.addEventListener('message', (event) => {
